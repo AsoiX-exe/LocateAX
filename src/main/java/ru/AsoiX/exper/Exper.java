3d976cad50ca5b2d24edd4bba1e5a513b3e893f8
AsoiX-exe <168535181+AsoiX-exe@users.noreply.github.com>
2024-08-20 00:22:07 +0300

    Initial commit

---
+++ src/main/java/ru/AsoiX/exper/Exper.java
@@ -0,0 +1,17 @@
+package ru.AsoiX.exper;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Exper extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
